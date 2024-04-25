3db3523189ec5b03ad5bb901ea3fa0eaab68e5d9
lewisccox <lc2071@hw.ac.uk>
2024-04-25 12:32:29 +0100

    Initialises the project

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+const now = new Date();
+
+
+function Header() {
+  return (
+  <div class="header">
+  <header>
+    <img src= "gratis-png-kirby-s-return-to-dream-land-kirby-s-dream-collection-kirby-s-dream-land-2-kirby-s-adventure-tornado.png" alt=""/> 
+    <h1>Pi</h1>
+    <img src= "286px-WiKirby_2010s_logo.png" alt=""/> 
+    <h1>ross</h1>
+  </header>
+  </div>
+  );
+}
+
+export default Header
